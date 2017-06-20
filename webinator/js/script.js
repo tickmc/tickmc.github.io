52531c3c6395ef1a8a70bedd57da9b71838931b4
tickmc <tickmc@gmail.com>
2017-06-20 06:27:37 +0100

    Create script.js

---
+++ webinator/js/script.js
@@ -0,0 +1,592 @@
+
+
+
+
+function addUp(form) {
+var LR_bookcaseV;
+LR_bookcaseV =parseInt(document.spacecalc1.LR_bookcase.value);
+if (isNaN(LR_bookcaseV) || LR_bookcaseV == "") {
+	LR_bookcaseV = 0;
+}
+
+var APP_aircondV;
+APP_aircondV =parseInt(document.spacecalc1.APP_aircond.value);
+if (isNaN(APP_aircondV) || APP_aircondV == "") {
+	APP_aircondV = 0;
+}
+
+var MISC_baby_carriageV;
+MISC_baby_carriageV =parseInt(document.spacecalc1.MISC_baby_carriage.value);
+if (isNaN(MISC_baby_carriageV) || MISC_baby_carriageV == "") {
+	MISC_baby_carriageV = 0;
+}
+
+var APP_dehumidV;
+APP_dehumidV =parseInt(document.spacecalc1.APP_dehumid.value);
+if (isNaN(APP_dehumidV) || APP_dehumidV == "") {
+	APP_dehumidV = 0;
+}
+
+var LR_bookshelvesV;
+LR_bookshelvesV =parseInt(document.spacecalc1.LR_bookshelves.value);
+if (isNaN(LR_bookshelvesV) || LR_bookshelvesV == "") {
+	LR_bookshelvesV = 0;
+}
+
+var MISC_bicycleV;
+MISC_bicycleV =parseInt(document.spacecalc1.MISC_bicycle.value);
+if (isNaN(MISC_bicycleV) || MISC_bicycleV == "") {
+	MISC_bicycleV = 0;
+}
+
+
+var LR_cabinetV;
+LR_cabinetV =parseInt(document.spacecalc1.LR_cabinet.value);
+if (isNaN(LR_cabinetV) || LR_cabinetV == "") {
+	LR_cabinetV = 0;
+}
+
+
+var APP_dishwasherV;
+APP_dishwasherV =parseInt(document.spacecalc1.APP_dishwasher.value);
+if (isNaN(APP_dishwasherV) || APP_dishwasherV == "") {
+	APP_dishwasherV = 0;
+}
+
+
+var MISC_deskV;
+MISC_deskV =parseInt(document.spacecalc1.MISC_desk.value);
+if (isNaN(MISC_deskV) || MISC_deskV == "") {
+	MISC_deskV = 0;
+}
+
+
+var LR_chair_armV;
+LR_chair_armV =parseInt(document.spacecalc1.LR_chair_arm.value);
+if (isNaN(LR_chair_armV) || LR_chair_armV == "") {
+	LR_chair_armV = 0;
+}
+
+
+var APP_freezerV;
+APP_freezerV =parseInt(document.spacecalc1.APP_freezer.value);
+if (isNaN(APP_freezerV) || APP_freezerV == "") {
+	APP_freezerV = 0;
+}
+
+
+var MISC_fanV;
+MISC_fanV =parseInt(document.spacecalc1.MISC_fan.value);
+if (isNaN(MISC_fanV) || MISC_fanV == "") {
+	MISC_fanV = 0;
+}
+
+
+var LR_chair_straightV;
+LR_chair_straightV =parseInt(document.spacecalc1.LR_chair_straight.value);
+if (isNaN(LR_chair_straightV) || LR_chair_straightV == "") {
+	LR_chair_straightV = 0;
+}
+
+
+var APP_stoveV;
+APP_stoveV =parseInt(document.spacecalc1.APP_stove.value);
+if (isNaN(APP_stoveV) || APP_stoveV == "") {
+	APP_stoveV = 0;
+}
+
+
+var MISC_filecab_cdbdV;
+MISC_filecab_cdbdV =parseInt(document.spacecalc1.MISC_filecab_cdbd.value);
+if (isNaN(MISC_filecab_cdbdV) || MISC_filecab_cdbdV == "") {
+	MISC_filecab_cdbdV = 0;
+}
+
+
+var LR_chair_occasV;
+LR_chair_occasV =parseInt(document.spacecalc1.LR_chair_occas.value);
+if (isNaN(LR_chair_occasV) || LR_chair_occasV == "") {
+	LR_chair_occasV = 0;
+}
+
+
+var APP_refridgeV;
+APP_refridgeV =parseInt(document.spacecalc1.APP_refridge.value);
+if (isNaN(APP_refridgeV) || APP_refridgeV == "") {
+	APP_refridgeV = 0;
+}
+
+
+var MISC_filecab_2V;
+MISC_filecab_2V =parseInt(document.spacecalc1.MISC_filecab_2.value);
+if (isNaN(MISC_filecab_2V) || MISC_filecab_2V == "") {
+	MISC_filecab_2V = 0;
+}
+
+
+var LR_chair_overstuffV;
+LR_chair_overstuffV =parseInt(document.spacecalc1.LR_chair_overstuff.value);
+if (isNaN(LR_chair_overstuffV) || LR_chair_overstuffV == "") {
+	LR_chair_overstuffV = 0;
+}
+
+
+var APP_vacuumV;
+APP_vacuumV =parseInt(document.spacecalc1.APP_vacuum.value);
+if (isNaN(APP_vacuumV) || APP_vacuumV == "") {
+	APP_vacuumV = 0;
+}
+
+
+var MISC_filecab_4V;
+MISC_filecab_4V =parseInt(document.spacecalc1.MISC_filecab_4.value);
+if (isNaN(MISC_filecab_4V) || MISC_filecab_4V == "") {
+	MISC_filecab_4V = 0;
+}
+
+
+var LR_chair_rockingV;
+LR_chair_rockingV =parseInt(document.spacecalc1.LR_chair_rocking.value);
+if (isNaN(LR_chair_rockingV) || LR_chair_rockingV == "") {
+	LR_chair_rockingV = 0;
+}
+
+
+var APP_washing_machV;
+APP_washing_machV =parseInt(document.spacecalc1.APP_washing_mach.value);
+if (isNaN(APP_washing_machV) || APP_washing_machV == "") {
+	APP_washing_machV = 0;
+}
+
+
+var MISC_golfbagV;
+MISC_golfbagV =parseInt(document.spacecalc1.MISC_golfbag.value);
+if (isNaN(MISC_golfbagV) || MISC_golfbagV == "") {
+	MISC_golfbagV = 0;
+}
+
+
+var LR_deskV;
+LR_deskV =parseInt(document.spacecalc1.LR_desk.value);
+if (isNaN(LR_deskV) || LR_deskV == "") {
+	LR_deskV = 0;
+}
+
+
+var APP_dryerV;
+APP_dryerV =parseInt(document.spacecalc1.APP_dryer.value);
+if (isNaN(APP_dryerV) || APP_dryerV == "") {
+	APP_dryerV = 0;
+}
+
+
+var MISC_hamperV;
+MISC_hamperV =parseInt(document.spacecalc1.MISC_hamper.value);
+if (isNaN(MISC_hamperV) || MISC_hamperV == "") {
+	MISC_hamperV = 0;
+}
+
+
+var LR_fireplace_equipV;
+LR_fireplace_equipV =parseInt(document.spacecalc1.LR_fireplace_equip.value);
+if (isNaN(LR_fireplace_equipV) || LR_fireplace_equipV == "") {
+	LR_fireplace_equipV = 0;
+}
+
+
+var APP_tableV;
+APP_tableV =parseInt(document.spacecalc1.APP_table.value);
+if (isNaN(APP_tableV) || APP_tableV == "") {
+	APP_tableV = 0;
+}
+
+
+var MISC_patio_tableV;
+MISC_patio_tableV =parseInt(document.spacecalc1.MISC_patio_table.value);
+if (isNaN(MISC_patio_tableV) || MISC_patio_tableV == "") {
+	MISC_patio_tableV = 0;
+}
+
+
+var LR_lampV;
+LR_lampV =parseInt(document.spacecalc1.LR_lamp.value);
+if (isNaN(LR_lampV) || LR_lampV == "") {
+	LR_lampV = 0;
+}
+
+
+var APP_chairV;
+APP_chairV =parseInt(document.spacecalc1.APP_chair.value);
+if (isNaN(APP_chairV) || APP_chairV == "") {
+	APP_chairV = 0;
+}
+
+
+var MISC_patio_chairV;
+MISC_patio_chairV =parseInt(document.spacecalc1.MISC_patio_chair.value);
+if (isNaN(MISC_patio_chairV) || MISC_patio_chairV == "") {
+	MISC_patio_chairV = 0;
+}
+
+
+var LR_sofa_6ftV;
+LR_sofa_6ftV =parseInt(document.spacecalc1.LR_sofa_6ft.value);
+if (isNaN(LR_sofa_6ftV) || LR_sofa_6ftV == "") {
+	LR_sofa_6ftV = 0;
+}
+
+
+var MISC_patio_loungeV;
+MISC_patio_loungeV =parseInt(document.spacecalc1.MISC_patio_lounge.value);
+if (isNaN(MISC_patio_loungeV) || MISC_patio_loungeV == "") {
+	MISC_patio_loungeV = 0;
+}
+
+
+var LR_sofa_sectV;
+LR_sofa_sectV =parseInt(document.spacecalc1.LR_sofa_sect.value);
+if (isNaN(LR_sofa_sectV) || LR_sofa_sectV == "") {
+	LR_sofa_sectV = 0;
+}
+
+
+var MISC_piano_babyV;
+MISC_piano_babyV =parseInt(document.spacecalc1.MISC_piano_baby.value);
+if (isNaN(MISC_piano_babyV) || MISC_piano_babyV == "") {
+	MISC_piano_babyV = 0;
+}
+
+
+var LR_sofa_loveseatV;
+LR_sofa_loveseatV =parseInt(document.spacecalc1.LR_sofa_loveseat.value);
+if (isNaN(LR_sofa_loveseatV) || LR_sofa_loveseatV == "") {
+	LR_sofa_loveseatV = 0;
+}
+
+
+var BR_bed_bunkV;
+BR_bed_bunkV =parseInt(document.spacecalc1.BR_bed_bunk.value);
+if (isNaN(BR_bed_bunkV) || BR_bed_bunkV == "") {
+	BR_bed_bunkV = 0;
+}
+
+
+var MISC_piano_upriteV;
+MISC_piano_upriteV =parseInt(document.spacecalc1.MISC_piano_uprite.value);
+if (isNaN(MISC_piano_upriteV) || MISC_piano_upriteV == "") {
+	MISC_piano_upriteV = 0;
+}
+
+
+var LR_stereo_consoleV;
+LR_stereo_consoleV =parseInt(document.spacecalc1.LR_stereo_console.value);
+if (isNaN(LR_stereo_consoleV) || LR_stereo_consoleV == "") {
+	LR_stereo_consoleV = 0;
+}
+
+
+var BR_bed_doubleV;
+BR_bed_doubleV =parseInt(document.spacecalc1.BR_bed_double.value);
+if (isNaN(BR_bed_doubleV) || BR_bed_doubleV == "") {
+	BR_bed_doubleV = 0;
+}
+
+
+var MISC_rug_largeV;
+MISC_rug_largeV =parseInt(document.spacecalc1.MISC_rug_large.value);
+if (isNaN(MISC_rug_largeV) || MISC_rug_largeV == "") {
+	MISC_rug_largeV = 0;
+}
+
+
+var LR_tableV;
+LR_tableV =parseInt(document.spacecalc1.LR_table.value);
+if (isNaN(LR_tableV) || LR_tableV == "") {
+	LR_tableV = 0;
+}
+
+
+var BR_bed_singleV;
+BR_bed_singleV =parseInt(document.spacecalc1.BR_bed_single.value);
+if (isNaN(BR_bed_singleV) || BR_bed_singleV == "") {
+	BR_bed_singleV = 0;
+}
+
+
+var MISC_rug_smallV;
+MISC_rug_smallV =parseInt(document.spacecalc1.MISC_rug_small.value);
+if (isNaN(MISC_rug_smallV) || MISC_rug_smallV == "") {
+	MISC_rug_smallV = 0;
+}
+
+
+var LR_table_coffee_endV;
+LR_table_coffee_endV =parseInt(document.spacecalc1.LR_table_coffee_end.value);
+if (isNaN(LR_table_coffee_endV) || LR_table_coffee_endV == "") {
+	LR_table_coffee_endV = 0;
+}
+
+
+var BR_bed_kingV;
+BR_bed_kingV =parseInt(document.spacecalc1.BR_bed_king.value);
+if (isNaN(BR_bed_kingV) || BR_bed_kingV == "") {
+	BR_bed_kingV = 0;
+}
+
+
+var MISC_sewing_machV;
+MISC_sewing_machV =parseInt(document.spacecalc1.MISC_sewing_mach.value);
+if (isNaN(MISC_sewing_machV) || MISC_sewing_machV == "") {
+	MISC_sewing_machV = 0;
+}
+
+
+var LR_tv_table_modelV;
+LR_tv_table_modelV =parseInt(document.spacecalc1.LR_tv_table_model.value);
+if (isNaN(LR_tv_table_modelV) || LR_tv_table_modelV == "") {
+	LR_tv_table_modelV = 0;
+}
+
+
+var BR_bed_queenV;
+BR_bed_queenV =parseInt(document.spacecalc1.BR_bed_queen.value);
+if (isNaN(BR_bed_queenV) || BR_bed_queenV == "") {
+	BR_bed_queenV = 0;
+}
+
+
+var MISC_suitcaseV;
+MISC_suitcaseV =parseInt(document.spacecalc1.MISC_suitcase.value);
+if (isNaN(MISC_suitcaseV) || MISC_suitcaseV == "") {
+	MISC_suitcaseV = 0;
+}
+
+
+var LR_tv_consoleV;
+LR_tv_consoleV =parseInt(document.spacecalc1.LR_tv_console.value);
+if (isNaN(LR_tv_consoleV) || LR_tv_consoleV == "") {
+	LR_tv_consoleV = 0;
+}
+
+
+var BR_bed_rollawayV;
+BR_bed_rollawayV =parseInt(document.spacecalc1.BR_bed_rollaway.value);
+if (isNaN(BR_bed_rollawayV) || BR_bed_rollawayV == "") {
+	BR_bed_rollawayV = 0;
+}
+
+
+var MISC_card_tableV;
+MISC_card_tableV =parseInt(document.spacecalc1.MISC_card_table.value);
+if (isNaN(MISC_card_tableV) || MISC_card_tableV == "") {
+	MISC_card_tableV = 0;
+}
+
+
+var LR_wallunitV;
+LR_wallunitV =parseInt(document.spacecalc1.LR_wallunit.value);
+if (isNaN(LR_wallunitV) || LR_wallunitV == "") {
+	LR_wallunitV = 0;
+}
+
+
+var BR_bookshelvesV;
+BR_bookshelvesV =parseInt(document.spacecalc1.BR_bookshelves.value);
+if (isNaN(BR_bookshelvesV) || BR_bookshelvesV == "") {
+	BR_bookshelvesV = 0;
+}
+
+
+var MISC_game_tableV;
+MISC_game_tableV =parseInt(document.spacecalc1.MISC_game_table.value);
+if (isNaN(MISC_game_tableV) || MISC_game_tableV == "") {
+	MISC_game_tableV = 0;
+}
+
+
+var BR_chairV;
+BR_chairV =parseInt(document.spacecalc1.BR_chair.value);
+if (isNaN(BR_chairV) || BR_chairV == "") {
+	BR_chairV = 0;
+}
+
+
+var MISC_trunkV;
+MISC_trunkV =parseInt(document.spacecalc1.MISC_trunk.value);
+if (isNaN(MISC_trunkV) || MISC_trunkV == "") {
+	MISC_trunkV = 0;
+}
+
+
+var BR_bureauV;
+BR_bureauV =parseInt(document.spacecalc1.BR_bureau.value);
+if (isNaN(BR_bureauV) || BR_bureauV == "") {
+	BR_bureauV = 0;
+}
+
+
+var MISC_box_bookV;
+MISC_box_bookV =parseInt(document.spacecalc1.MISC_box_book.value);
+if (isNaN(MISC_box_bookV) || MISC_box_bookV == "") {
+	MISC_box_bookV = 0;
+}
+
+
+var DR_buffet_baseV;
+DR_buffet_baseV =parseInt(document.spacecalc1.DR_buffet_base.value);
+if (isNaN(DR_buffet_baseV) || DR_buffet_baseV == "") {
+	DR_buffet_baseV = 0;
+}
+
+
+var BR_chest_cedarV;
+BR_chest_cedarV =parseInt(document.spacecalc1.BR_chest_cedar.value);
+if (isNaN(BR_chest_cedarV) || BR_chest_cedarV == "") {
+	BR_chest_cedarV = 0;
+}
+
+
+var MISC_box_legalV;
+MISC_box_legalV =parseInt(document.spacecalc1.MISC_box_legal.value);
+if (isNaN(MISC_box_legalV) || MISC_box_legalV == "") {
+	MISC_box_legalV = 0;
+}
+
+
+var DR_cabinet_cornerV;
+DR_cabinet_cornerV =parseInt(document.spacecalc1.DR_cabinet_corner.value);
+if (isNaN(DR_cabinet_cornerV) || DR_cabinet_cornerV == "") {
+	DR_cabinet_cornerV = 0;
+}
+
+
+var BR_deskV;
+BR_deskV =parseInt(document.spacecalc1.BR_desk.value);
+if (isNaN(BR_deskV) || BR_deskV == "") {
+	BR_deskV = 0;
+}
+
+
+var MISC_box_allpurpV;
+MISC_box_allpurpV =parseInt(document.spacecalc1.MISC_box_allpurp.value);
+if (isNaN(MISC_box_allpurpV) || MISC_box_allpurpV == "") {
+	MISC_box_allpurpV = 0;
+}
+
+
+var table_diningV;
+table_diningV =parseInt(document.spacecalc1.table_dining.value);
+if (isNaN(table_diningV) || table_diningV == "") {
+	table_diningV = 0;
+}
+
+
+var BR_dresser_singleV;
+BR_dresser_singleV =parseInt(document.spacecalc1.BR_dresser_single.value);
+if (isNaN(BR_dresser_singleV) || BR_dresser_singleV == "") {
+	BR_dresser_singleV = 0;
+}
+
+
+var MISC_box_wardrobeV;
+MISC_box_wardrobeV =parseInt(document.spacecalc1.MISC_box_wardrobe.value);
+if (isNaN(MISC_box_wardrobeV) || MISC_box_wardrobeV == "") {
+	MISC_box_wardrobeV = 0;
+}
+
+
+var DR_chair_diningV;
+DR_chair_diningV =parseInt(document.spacecalc1.DR_chair_dining.value);
+if (isNaN(DR_chair_diningV) || DR_chair_diningV == "") {
+	DR_chair_diningV = 0;
+}
+
+
+var BR_dresser_doubleV;
+BR_dresser_doubleV =parseInt(document.spacecalc1.BR_dresser_double.value);
+if (isNaN(BR_dresser_doubleV) || BR_dresser_doubleV == "") {
+	BR_dresser_doubleV = 0;
+}
+
+
+var DR_serverV;
+DR_serverV =parseInt(document.spacecalc1.DR_server.value);
+if (isNaN(DR_serverV) || DR_serverV == "") {
+	DR_serverV = 0;
+}
+
+
+var BR_nightstandV;
+BR_nightstandV =parseInt(document.spacecalc1.BR_nightstand.value);
+if (isNaN(BR_nightstandV) || BR_nightstandV == "") {
+	BR_nightstandV = 0;
+}
+
+
+var BR_armoireV;
+BR_armoireV =parseInt(document.spacecalc1.BR_armoire.value);
+if (isNaN(BR_armoireV) || BR_armoireV == "") {
+	BR_armoireV = 0;
+}
+
+
+
+var newTot;
+newTot = (LR_bookcaseV * 20) + (APP_aircondV * 20) + (MISC_baby_carriageV *
+4) + (LR_bookshelvesV * 5) + (APP_dehumidV * 10) + (MISC_bicycleV * 5) +
+(LR_cabinetV * 10) + (APP_dishwasherV * 20) + (MISC_deskV * 30) +
+(LR_chair_armV * 10) + (APP_freezerV * 45) + (MISC_fanV * 5) +
+(LR_chair_straightV * 5) + (APP_stoveV * 15) + (MISC_filecab_cdbdV * 3) +
+(LR_chair_occasV * 15) + (APP_refridgeV * 45) + (MISC_filecab_2V * 10) +
+(LR_chair_overstuffV * 25) + (APP_vacuumV * 5) + (MISC_filecab_4V * 20) +
+(LR_chair_rockingV * 15) + (APP_washing_machV * 25) + (MISC_golfbagV * 4) +
+(LR_deskV * 35) + (APP_dryerV * 25) + (MISC_hamperV * 5) +
+(LR_fireplace_equipV * 5) + (APP_tableV * 10) + (MISC_patio_tableV * 1) +
+(LR_lampV * 3) + (APP_chairV * 5) + (MISC_patio_chairV * 6) + (LR_sofa_6ftV
+* 50) + (MISC_patio_loungeV * 35) + (LR_sofa_sectV * 30) +
+(MISC_piano_babyV * 75) + (LR_sofa_loveseatV * 35) + (BR_bed_bunkV * 70) +
+(MISC_piano_upriteV * 60) + (LR_stereo_consoleV * 15) + (BR_bed_doubleV *
+60) + (MISC_rug_largeV * 15) + (LR_tableV * 12) + (BR_bed_singleV * 40) +
+(MISC_rug_smallV * 5) + (LR_table_coffee_endV * 5) + (BR_bed_kingV * 70) +
+(MISC_sewing_machV * 10) + (LR_tv_table_modelV * 10) + (BR_bed_queenV * 65)
++ (MISC_suitcaseV * 5) + (LR_tv_consoleV * 15) + (BR_bed_rollawayV * 20) +
+(MISC_card_tableV * 2) + (LR_wallunitV * 36) + (BR_bookshelvesV * 5) +
+(MISC_game_tableV * 15) + (BR_chairV * 5) + (MISC_trunkV * 5) +
+(BR_bureauV * 12) + (MISC_box_bookV * 1.5) + (DR_buffet_baseV * 30) +
+(BR_chest_cedarV * 15) + (MISC_box_legalV * 2.5) + (DR_cabinet_cornerV *
+20) + (BR_deskV * 22) + (MISC_box_allpurpV * 3) + (table_diningV * 30) +
+(BR_dresser_singleV * 20) + (MISC_box_wardrobeV * 16) + (DR_chair_diningV *
+8) + (BR_dresser_doubleV * 40) + (DR_serverV * 15) + (BR_nightstandV * 5) +
+(BR_armoireV * 40);
+newTot = newTot * 1.15;
+
+newTot = Math.round(newTot);
+  
+  
+ 
+   if (newTot <= 200) {
+    newTot = "5 x 5";
+   
+  } else if (newTot <=400 && newTot > 200) {
+    newTot = "5 x 10";
+  } else if (newTot <=800 && newTot > 400) {
+    newTot = "10 x 10";
+  } else if (newTot <=1200 && newTot > 800) {
+    newTot = "10 x 15";
+  } else if (newTot <=1600 && newTot > 1200) {
+    newTot = "10 x 20";
+  } else if (newTot <=2000 && newTot > 1600) {
+    newTot = "10 x 25";
+  };
+
+   
+ document.spacecalc1.totalfield.value = newTot;
+  
+  
+  
+
+}
+ 
+   
+
+
